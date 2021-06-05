e4a022c66f7faa27ba06ffb356b0f75279edc047
Tejal Badgujar <57011203+tejalbadgujar@users.noreply.github.com>
2021-06-05 21:12:01 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa non modi
+        delectus deserunt. Amet quasi rerum harum incidunt. Facere porro
+        exercitationem iusto nesciunt similique nostrum eaque expedita magni
+        distinctio nulla..
+      </p>
+    </div>
+  );
+}
+
+export default Note;
